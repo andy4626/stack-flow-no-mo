289a0362f53c91808e763829168304c37dffb5c5
David Louie <dlouie1492@gmail.com>
2016-03-04 15:55:30 -0500

    Add new question form

--- stack-flow-no-mo/app/controllers/questions_controller.rb
+++ stack-flow-no-mo/app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   def index
-    @questions = Question.all
+    @questions = Question.order(updated_at: :desc)
   end
 
   def new
@@ -8,9 +8,9 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    if logged_in?
-      @question.create(get_params)
-      redirect_to question_show_path(@question)
+    @question = Question.new(get_params)
+    if logged_in? && @question.save
+      redirect_to question_path(@question)
     else
       redirect_to :back
     end
