7548b3ee1d589dc477a10ec300b56401a3fa08d0
David Louie <dlouie1492@gmail.com>
2016-03-06 20:16:36 -0500

    Added guard clause to votes so users who are not logged in cannot vote

--- stack-flow-no-mo/app/controllers/votes_controller.rb
+++ stack-flow-no-mo/app/controllers/votes_controller.rb
@@ -1,46 +1,64 @@
 class VotesController < ApplicationController
   def q_upvote
     @question = Question.find(params[:question_id])
-    if voted?(@question)
-      vote = @question.votes.find_by(user: current_user)
-      vote.update_attributes(value: 1)
+    if logged_in?
+      if voted?(@question)
+        vote = @question.votes.find_by(user: current_user)
+        vote.update_attributes(value: 1)
+      else
+        @question.votes.create(user: current_user, value: 1)
+      end
+      redirect_to :back
     else
-      @question.votes.create(user: current_user, value: 1)
+      redirect_to :back
     end
-    redirect_to :back
   end
 
   def q_downvote
     @question = Question.find(params[:question_id])
-    if voted?(@question)
-      vote = @question.votes.find_by(user: current_user)
-      vote.update_attributes(value: -1)
+    if logged_in?
+      if voted?(@question)
+        vote = @question.votes.find_by(user: current_user)
+        vote.update_attributes(value: -1)
+      else
+        @question.votes.create(user: current_user, value: -1)
+      end
+      redirect_to :back
     else
-      @question.votes.create(user: current_user, value: -1)
+      redirect_to :back
     end
-    redirect_to :back
   end
 
   def a_upvote
     @answer = Answer.find(params[:answer_id])
-    if voted?(@answer)
-      vote = @answer.votes.find_by(user: current_user)
-      vote.update_attributes(value: 1)
+    if logged_in?
+      if voted?(@answer)
+        vote = @answer.votes.find_by(user: current_user)
+        vote.update_attributes(value: 1)
+      else
+        @answer.votes.create(user: current_user, value: 1)
+      end
+      redirect_to :back
     else
-      @answer.votes.create(user: current_user, value: 1)
+      redirect_to :back
     end
-    redirect_to :back
   end
 
   def a_downvote
     @answer = Answer.find(params[:answer_id])
-    if voted?(@answer)
-      vote = @answer.votes.find_by(user: current_user)
-      vote.update_attributes(value: -1)
+    if logged_in?
+      if voted?(@answer)
+        vote = @answer.votes.find_by(user: current_user)
+        if logged_in?
+        vote.update_attributes(value: -1)
+        else
+          @answer.votes.create(user: current_user, value: -1)
+        end
+      redirect_to :back
+      end
     else
-      @answer.votes.create(user: current_user, value: -1)
+      redirect_to :back
     end
-    redirect_to :back
   end
 
   private
